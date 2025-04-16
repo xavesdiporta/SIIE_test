55b93a0418235bcdea1f2ec2cc2b84722ea5bb19
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2025-04-16 18:02:20 +0100

    asd

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app', [
+    'namePage' => 'Dashboard',
+    'class' => 'login-page sidebar-mini ',
+    'activePage' => 'home',
+    'backgroundImage' => asset('now') . "/img/bg14.jpg",
+])
+
+@section('content')
+  <div class="panel-header panel-header-lg">
+    <canvas id="bigDashboardChart"></canvas>
+  </div>
+  <div class="content">
+  </div>
+@endsection
+
+@push('js')
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/js/demos.js
+      demo.initDashboardPageCharts();
+
+    });
+  </script>
+@endpush
